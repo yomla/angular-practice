e33485b54be63d9a4cfe9dc3f03ee65cbcea70fb
Mladen <djoml8bre@gmail.com>
2018-01-03 16:32:43 +0100

    Commit-5 Selector types

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  // selector: '[app-servers]', // Atribute selector(selecting by html element atribute)
+  // selector: '.app-servers', // selecting by html class atribute, sidenote selecting by ID isn't suported by Angular
+  selector: 'app-servers', 
   template: `
     <app-server></app-server>
     <app-server></app-server>
