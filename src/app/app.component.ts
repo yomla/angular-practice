3f4518bce11fff3cd66db9625985363210fa7f79
Mladen <djoml8bre@gmail.com>
2018-01-03 15:54:18 +0100

    Commit-4 Adding styles, external and inline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css'] // Note: It's an array -may have multiple style files
+  styles: [`
+    h3 {
+      color: dodgerblue;
+    }
+  `] // Inline styles
 })
 export class AppComponent {
 }
